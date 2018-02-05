783bc7089ffc19facf46c60fc4e4c2861443570c
Jackoo2162 <35918848+Jackoo2162@users.noreply.github.com>
2018-02-05 19:05:46 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+
+
+bot.on('message', message => {
+  if (message.content === '') {
+   // message.reply('pong');
+    message.channel.sendMessage('');
+  }
+});
+
+bot.on("ready", () => {
+  bot.user.setPresence({game: {name:""}})
+  console.log('I have jackoo: {SERVERNAME}')
+})
+
+bot.on('ready', ()=>{
+  console.log(`Logged in as ${bot.user.tag}!`);
+  voiceChannel = bot.guilds.find('name', 'DREAMENERG3T1CER').channels.find('name', '');
+  voiceChannel.join().then(()=>{
+      console.log(`Connected to ${voiceChannel.name}!`);
+  });
+});
+
+
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
+
+
+bot.login(process.env.TOKEN);
